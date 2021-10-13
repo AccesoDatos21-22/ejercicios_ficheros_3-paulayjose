9540c877d5ccdf6a3241e5f5f981adc4e4cf85db
Paulaccz <paucabcan01@gmail.com>
2021-10-13 10:51:03 +0200

    Ejercicio_14

--- Main.java
+++ Main.java
@@ -18,54 +18,57 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Ejercicio 14 System.out.println("EJERCICIO 1"); System.out.println();
-		 * 
-		 * List<Pokemon> lista = new ArrayList<Pokemon>(); String f = "Pokemones.csv";
-		 * 
-		 * Pokemon b = new Pokemon("Bulbasaur", 3, 10, 34, 41, 45, 55, 24); Pokemon s =
-		 * new Pokemon("Squirtle", 4, 13, 39, 36, 49, 51, 28); Pokemon c = new
-		 * Pokemon("Charmander", 5, 16, 41, 35, 55, 47, 35);
-		 * 
-		 * PokemonDAOImp uno = new PokemonDAOImp(3); uno.escribirPokemon(f,
-		 * b.getNombre(), b.getNivel(), b.getVida(), b.getAtaque(), b.getDefensa(),
-		 * b.getAtaque_especial(), b.getDefensa_especial(), b.getVelocidad());
-		 * uno.escribirPokemon(f, s.getNombre(), s.getNivel(), s.getVida(),
-		 * s.getAtaque(), s.getDefensa(), s.getAtaque_especial(),
-		 * s.getDefensa_especial(), s.getVelocidad()); uno.escribirPokemon(f,
-		 * c.getNombre(), c.getNivel(), c.getVida(), c.getAtaque(), c.getDefensa(),
-		 * c.getAtaque_especial(), c.getDefensa_especial(), c.getVelocidad());
-		 * 
-		 * uno.imprimirPokemon(f);
-		 * 
-		 * lista = uno.leerPokemon(f);
-		 * 
-		 * for (Pokemon pokemon : lista) { System.out.println(pokemon); }
-		 */
+		// Ejercicio 14
+		System.out.println("EJERCICIO 1");
+		System.out.println();
 
-		Scanner sc = new Scanner(System.in);
+		List<Pokemon> lista = new ArrayList<Pokemon>();
+		String f = "Pokemones.csv";
 
-		Ejercicios1_3 ej = new Ejercicios1_3();
+		Pokemon b = new Pokemon("Bulbasaur", 3, 10, 34, 41, 45, 55, 24);
+		Pokemon s = new Pokemon("Squirtle", 4, 13, 39, 36, 49, 51, 28);
+		Pokemon c = new Pokemon("Charmander", 5, 16, 41, 35, 55, 47, 35);
 
-		String ruta = "Ejercicio_10.dat";
+		PokemonDAOImp uno = new PokemonDAOImp(3);
+		uno.escribirPokemon(f, b.getNombre(), b.getNivel(), b.getVida(), b.getAtaque(), b.getDefensa(),
+				b.getAtaque_especial(), b.getDefensa_especial(), b.getVelocidad());
+		uno.escribirPokemon(f, s.getNombre(), s.getNivel(), s.getVida(), s.getAtaque(), s.getDefensa(),
+				s.getAtaque_especial(), s.getDefensa_especial(), s.getVelocidad());
+		uno.escribirPokemon(f, c.getNombre(), c.getNivel(), c.getVida(), c.getAtaque(), c.getDefensa(),
+				c.getAtaque_especial(), c.getDefensa_especial(), c.getVelocidad());
 
-		System.out.println("¿Cuántos números desea introducir?");
-		int cant = sc.nextInt();
-		sc.nextLine();
+		//uno.imprimirPokemon(f);
 
-		for (int i = 0; i < cant; i++) {
+		lista = uno.leerPokemon(f);
 
-			System.out.print("introduzca un número: ");
-			float num = sc.nextFloat();
-
-			ej.escribirFlotante(num, ruta);
-
+		for (Pokemon pokemon : lista) {
+			System.out.println(pokemon);
 		}
 
-		System.out.println("\nLos números del fichero son: ");
-		ej.imprimirFlotante(ruta);
-
-		sc.close();
+		// Ejercicio 10
+		/*
+		 * Scanner sc = new Scanner(System.in);
+		 * 
+		 * Ejercicios1_3 ej = new Ejercicios1_3();
+		 * 
+		 * String ruta = "Ejercicio_10.dat";
+		 * 
+		 * System.out.println("¿Cuántos números desea introducir?"); int cant =
+		 * sc.nextInt(); sc.nextLine();
+		 * 
+		 * for (int i = 0; i < cant; i++) {
+		 * 
+		 * System.out.print("introduzca un número: "); float num = sc.nextFloat();
+		 * 
+		 * ej.escribirFlotante(num, ruta);
+		 * 
+		 * }
+		 * 
+		 * System.out.println("\nLos números del fichero son: ");
+		 * ej.imprimirFlotante(ruta);
+		 * 
+		 * sc.close();
+		 */
 
 	}
 }
